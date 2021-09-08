b7e2dc9899620fba831660fdeac752f71d28021f
Boss <C:\Users\\AppData\Roaming\The Bat!>
2021-09-09 01:29:50 +0300

    feat: add footer with styles

--- rs-shop/src/app/core/core.module.ts
+++ rs-shop/src/app/core/core.module.ts
@@ -5,6 +5,7 @@ import { InfoBlockComponent } from './components/info-block/info-block.component
 import { HeaderComponent } from './pages/header/header.component';
 import { MainMenuComponent } from './components/main-menu/main-menu.component';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { FooterComponent } from './pages/footer/footer.component';
 
 @NgModule({
   declarations: [
@@ -12,9 +13,13 @@ import { NavBarComponent } from './components/nav-bar/nav-bar.component';
     HeaderComponent,
     MainMenuComponent,
     NavBarComponent,
+    FooterComponent,
   ],
   imports: [SharedModule],
-  exports: [HeaderComponent],
+  exports: [
+    HeaderComponent,
+    FooterComponent,
+  ],
   providers: [LocationService],
 })
 export class CoreModule { }
